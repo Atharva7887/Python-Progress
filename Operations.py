1d7acbbc885af72b0b2d7930017cb0dc47239858
Atharva7887 <athshirke2001@gmail.com>
2025-04-17 19:15:42 +0530

    Updated variable.py

---
+++ Operations.py
@@ -0,0 +1,11 @@
+a=int(input("Enter number 1: "))
+b=int(input("Enter number 2: "))
+
+# Arithmetic operations
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Floor Division:", a//b)
+print("Modulus:", a%b)
+print("Exponentiation:", a**b)
