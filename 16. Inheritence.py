a06a02f6832bf4b12bb8c81745e133c049b14ea1
Atharva7887 <athshirke2002@gmail.com>
2025-04-21 16:26:21 +0530

    Updated Inheritance.py

--- 16. Inheritence.py
+++ 16. Inheritence.py
@@ -9,16 +9,13 @@ class employee:
         print(f"The name is {self.name} and salary is {self.salary}")
 
 
-class programer:
-    company="ITC Info"
-    def show(self):
-        print(f"The name is {self.name} and salary is {self.salary}")
+class programer(employee): #Inherits from employee
+    
 
-    def showlang(self):
-        print(f"The name is {self.name} and he is good with {self.language}") 
+    def showlang(self, language):
+        print(f"The name is {self.name} and he is good with {language}") 
 
-a=employee()
 b=programer()
-
-print(f"{a.name}\n{b.company}")
-a.show()
+print(b.name)
+b.show()
+b.showlang("Python")
