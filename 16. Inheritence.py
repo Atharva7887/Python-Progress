373a79efb00defc76bbd1184975dd4063919ee0d
Atharva7887 <athshirke2002@gmail.com>
2025-04-24 21:58:46 +0530

    Added Property decoreater

--- 16. Inheritence.py
+++ 16. Inheritence.py
@@ -1,3 +1,4 @@
+#part 1
 class employee: #Parent class
     
     name="Atharva Shirke"
@@ -20,3 +21,34 @@ b=programer()
 print(b.name)
 b.show()
 b.showlang("Python")
+
+class company(programer): #Inherits from programer, multiply level inheritance
+    name="Infosys"
+
+    def department(self):
+        print(f"The name is {self.name} and he is in the IT department")
+
+
+
+#Part 2
+class Employee:
+    a=1
+    def __init__(self):
+        print("Employee constructor")
+
+class Department(Employee): #Single classInheritance, 
+    b=2
+    def __init__(self):
+        super().__init__()
+        print("Department constructor") #Super constructor is called first
+
+class Company(Department): #
+    c=3
+    def __init__(self):
+        super().__init__()
+        print("Company constructor")
+
+#class Project(Company,Department): #Multiple class inheritance
+
+d=Company()
+print(d.a,d.b,d.c)
